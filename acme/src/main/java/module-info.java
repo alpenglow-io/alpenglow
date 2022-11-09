b36804f32c04dbd6ab3272bfc6f7d97fcf86cb69
Luca Guadagnini <luca.guadagnini@gmail.com>
2022-11-09 01:17:06 +0100

    added tests for acme client

---
+++ acme/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module io.alpenglow.acme {
+  requires org.bouncycastle.provider;
+  requires org.shredzone.acme4j;
+  requires org.shredzone.acme4j.utils;
+  requires org.slf4j;
+  requires java.desktop;
+  requires org.apache.logging.log4j;
+  requires org.apache.logging.log4j.core;
+  requires org.apache.logging.log4j.slf4j;
+}
